8e115a507c88413b4a724771e3bc52d9687ea054
SHISEN <alex-ench@yandex.ru>
2022-05-03 07:02:39 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,19 @@ import DemoExam from "./components/Student/DemoExam";
 import Employees from "./components/Employees";
 import Teacher from "./components/Employees/Teacher";
 
+import Shedule from "./components/Schedule";
+
+import InfoAboutOrganization from "./components/InfoAboutOrganization";
+import BasicInfo from "./components/InfoAboutOrganization/BasicInfo";
+import StructureManagement from "./components/InfoAboutOrganization/StructureManagement";
+import Documents from "./components/InfoAboutOrganization/Documents";
+import Education from "./components/InfoAboutOrganization/Education";
+import EducationalStandards from "./components/InfoAboutOrganization/EducationalStandards";
+import LeadershipTeachers from "./components/InfoAboutOrganization/LeadershipTeachers";
+import Leadership from "./components/InfoAboutOrganization/Leadership";
+import Teachers from "./components/InfoAboutOrganization/Teachers";
+import LogisticsEquipment from "./components/InfoAboutOrganization/LogisticsEquipment";
+
 function App() {
   const [stylePath, setStylePath] = useState(null);
   return (
@@ -41,15 +54,30 @@ function App() {
             <Route path="tuition_price" element={<TuitionPrice setStylePath={setStylePath} />} />
           </Route>
 
-          <Route path="/student" element={<Student setStylePath={setStylePath} />} />
-          <Route path="/student/title_pages_for_final_qualifying_work" element={<TitlePagesForFinalQualifyingWork setStylePath={setStylePath} />} />
-          <Route path="/student/portfolio" element={<Portfolio setStylePath={setStylePath} />} />
-          <Route path="/student/release_information" element={<ReleaseInformation setStylePath={setStylePath} />} />
-          <Route path="/student/methodical_materials" element={<MethodicalMaterials setStylePath={setStylePath} />} />
-          <Route path="/student/demo_exam" element={<DemoExam setStylePath={setStylePath} />} />
+          <Route path="/student" element={<Student setStylePath={setStylePath} />}>
+            <Route path="title_pages_for_final_qualifying_work" element={<TitlePagesForFinalQualifyingWork setStylePath={setStylePath} />} />
+            <Route path="portfolio" element={<Portfolio setStylePath={setStylePath} />} />
+            <Route path="release_information" element={<ReleaseInformation setStylePath={setStylePath} />} />
+            <Route path="methodical_materials" element={<MethodicalMaterials setStylePath={setStylePath} />} />
+            <Route path="demo_exam" element={<DemoExam setStylePath={setStylePath} />} />
+          </Route>
 
           <Route path="/employees" element={<Employees setStylePath={setStylePath} />} />
           <Route path="/employees/teacher" element={<Teacher setStylePath={setStylePath} />} />
+
+          <Route path="/shedule" element={<Shedule setStylePath={setStylePath} />} />
+
+          <Route path="/info_about_organization" element={<InfoAboutOrganization setStylePath={setStylePath} />} />
+          <Route path="/info_about_organization/basic_info" element={<BasicInfo setStylePath={setStylePath} />} />
+          <Route path="/info_about_organization/structure_management" element={<StructureManagement setStylePath={setStylePath} />} />
+          <Route path="/info_about_organization/documents" element={<Documents setStylePath={setStylePath} />} />
+          <Route path="/info_about_organization/education" element={<Education setStylePath={setStylePath} />} />
+          <Route path="/info_about_organization/educational_standarts" element={<EducationalStandards setStylePath={setStylePath} />} />
+          <Route path="/info_about_organization/leadership_teachers" element={<LeadershipTeachers setStylePath={setStylePath} />}>
+            <Route path="leadership" element={<Leadership setStylePath={setStylePath} />} />
+            <Route path="teachers" element={<Teachers setStylePath={setStylePath} />} />
+          </Route>
+          <Route path="/info_about_organization/logistics_equipment" element={<LogisticsEquipment setStylePath={setStylePath} />} />
         </Routes>
       </BrowserRouter>
       <Footer />
